5476d7f321c2f0a4d367ea9983def084863e957a
Denrusn <45091155+Denrusn@users.noreply.github.com>
2024-01-03 15:57:48 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,7 @@
-from flask import Flask, jsonify
 
-app = Flask(__name__)
-
-
-@app.route("/")
 def homepage():
-    return jsonify({"response": "hello word"})
+    return "helllllllo   word"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    homepage()
