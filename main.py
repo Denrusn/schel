1408ee2ab9aae4232decdff0cbb884d675938906
Denrusn <45091155+Denrusn@users.noreply.github.com>
2024-01-08 10:37:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -181,4 +181,4 @@ if __name__ == '__main__':
     # h_obj = HupuSpider()
     # h_obj.home_page()
     z_obj = Zhiboba()
-    z_obj.main(game_id='1231127', sid_no_change_limit=30)
+    z_obj.main(game_id='1231127', sid_no_change_limit=90)
