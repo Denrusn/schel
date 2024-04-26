1df8e75c1be0886f032a0406f070f525f9b16fd5
Denrusn <45091155+Denrusn@users.noreply.github.com>
2024-04-26 09:04:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -196,4 +196,4 @@ if __name__ == '__main__':
     utc_today = datetime.now()
     beijing_today = utc_today.astimezone(shanghai_tz).strftime("%Y-%m-%d")
     z_obj = Zhiboba()
-    z_obj.main(date=beijing_today, game_id='1405650', sid_no_change_limit=200)
+    z_obj.main(date=beijing_today, game_id='1405665', sid_no_change_limit=200)
